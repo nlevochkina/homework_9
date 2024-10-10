7fe362d70e88181f62a1699707f7c04880a31f26
natalia <natalia22levochkina@gmail.com>
2024-10-10 22:31:16 +0300

    test

--- homework_9/tests/test_fill_inputs.py
+++ homework_9/tests/test_fill_inputs.py
@@ -9,20 +9,6 @@ def test_fill_out_the_form():
     # WHEN
     registration_page.register_user(users.user)
 
-    # registration_page.fill_full_name('Natalia', 'La')
-    # registration_page.fill_email('natalia@mail.ru')
-    # registration_page.choose_gender('label[for="gender-radio-2"]')
-    # registration_page.fill_phone('1234567890')
-    # registration_page.select_date_of_birth('react-datepicker__day--012',
-    #                                        "//*[@class='react-datepicker__month-select']//option[@value='0']",
-    #                                        "//*[@class='react-datepicker__year-select']//option[@value='1990']")
-    # registration_page.fill_subject('Computer Science')
-    # registration_page.choose_hobby('label[for="hobbies-checkbox-1"]')
-    # registration_page.fill_adress('Moscow Street, 56, Moscow, Russia')
-    # registration_page.upload_picture('../tests/resources/sun.jpeg')
-    # registration_page.select_state_and_city('Haryana', 'Karnal')
-    # registration_page.submit()
-
     # THEN
     registration_page.should_registered_user_with(users.user)
 
