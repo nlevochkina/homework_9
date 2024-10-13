6964087b0878b6f53176523658640b5bdb1d6d23
natalia <natalia22levochkina@gmail.com>
2024-10-13 17:58:32 +0300

    Поправила размер браузера

--- homework_9/tests/conftest.py
+++ homework_9/tests/conftest.py
@@ -14,5 +14,5 @@ def open_browser():
 @pytest.fixture(scope="function", autouse=True)
 def set_browser_size():
     browser.config.driver_name = "chrome"
-    browser.config.window_height = 800
-    browser.config.window_width = 800
+    browser.config.window_height = 1920
+    browser.config.window_width = 1080
