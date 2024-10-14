3eca864875eab47f460de5e9c4b1334b987010ad
nlevochkina <nlevochkina@ozon.ru>
2024-10-14 09:21:06 +0300

    Поменяла ширину и длину экрана

--- homework_9/tests/conftest.py
+++ homework_9/tests/conftest.py
@@ -14,5 +14,5 @@ def open_browser():
 @pytest.fixture(scope="function", autouse=True)
 def set_browser_size():
     browser.config.driver_name = "chrome"
-    browser.config.window_height = 1920
-    browser.config.window_width = 1080
+    browser.config.window_height = 1080
+    browser.config.window_width = 1920
